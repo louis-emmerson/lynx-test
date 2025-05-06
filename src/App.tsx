1e8268a4c46cc261809e2c337ce99006f1222215
Louis Emmerson <louis.emmerson2000@gmail.com>
2025-05-06 20:34:47 +0100

    counting button

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,28 @@ import arrow from "./assets/arrow.png";
 import lynxLogo from "./assets/lynx-logo.png";
 import reactLynxLogo from "./assets/react-logo.png";
 
-export function App(props: { onMounted?: () => void }) {
-  const [alterLogo, setAlterLogo] = useState(false);
+export function App() {
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     console.info("Hello, ReactLynx");
-    props.onMounted?.();
   }, []);
 
-  const onTap = useCallback(() => {
-    "background only";
-    setAlterLogo(!alterLogo);
-  }, [alterLogo]);
+  const handleClick = () => {
+    console.info("Button clicked");
+    setCount((prevCount) => prevCount + 1);
+  };
 
   return (
-    <view className="bg-white h-full flex items-center justify-center">
+    <view className="bg-white h-full flex flex-col  items-center justify-center">
       <text className="text-2xl text-center text-gray-900">Hello, LynxJS!</text>
+      <view
+        className="flex flex-col items-center justify-center mt-4 p-4 rounded-xl bg-gray-400"
+        bindtap={handleClick}
+      >
+        <text>Click Me!</text>
+        <text>{count}</text>
+      </view>
     </view>
   );
 }
